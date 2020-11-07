5c86ed22883f778401b7079459bf0014dd0ddf60
Moiz Qureshi <30798716+imoizuddin@users.noreply.github.com>
2020-11-07 19:15:48 +0530

    added a class for listing todos

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ) { }
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -8,9 +17,12 @@ import { Component, OnInit } from '@angular/core';
 export class ListTodosComponent implements OnInit {
 
   todos = [
-    {id : 1, description : 'Learn to Dance'},
-    {id : 2, description : 'Become an Expert at Angular'},
-    {id : 3, description : 'Visit India'}
+    new Todo(1, 'Learn to Code', false, new Date()),
+    new Todo(2, 'Become an Expert in JavaScript', false, new Date()),
+    new Todo(3, 'Become an Expert in Angular', false, new Date())
+    // {id : 1, description : 'Learn to Dance'},
+    // {id : 2, description : 'Become an Expert at Angular'},
+    // {id : 3, description : 'Visit India'}
   ]
 
   constructor() { }
